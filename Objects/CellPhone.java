8d6e7c4caaad1d5d7547b20512d67cd8bba5a33a
Mr. Wiebe <MrWiebe@users.noreply.github.com>
2018-09-05 12:06:42 -0700

    Added Cellphone

---
+++ Objects/CellPhone.java
@@ -0,0 +1,34 @@
+
+/**
+ * Write a description of class CellPhone here.
+ *
+ * @author Wiebe
+ * @version 9.4.18
+ */
+public class CellPhone{
+    private int screenWidth;
+    private int screenHeight;
+    private String color;
+    private String chargePort;
+    private boolean camera;
+    
+    public CellPhone(int screenW, int screenH, String colour, String chargeP, boolean cam){
+        screenWidth = screenW;
+        screenHeight = screenH;
+        color = colour;
+        chargePort = chargeP;
+        camera = cam;
+    }
+    
+    public void call(String name){
+        System.out.println("You call " + name);
+    }
+    
+    public void compute(){
+        System.out.println("You compute stuff");
+    }
+    
+    public static void getOtherPhoneNumber(){
+        System.out.println("You get their digits");
+    }
+}
